e1852fd73297cb29fc86b9181ec5867602fb9ee1
iPieter <pieter938@gmail.com>
2015-02-26 16:27:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>Autobiografie</title>
+
+	<link href="styles.css" rel="stylesheet" type= "text/css" >
+	<link href='http://fonts.googleapis.com/css?family=EB+Garamond&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<div id = "topbar" >
+	
+	<?php  
+	$name = "biograph";
+	include("topbar.php"); ?>
+	  
+</div>
+	
+<div class= "container" >
+		 
+	<h1>Biografie</h1>
+		 
+	<p>Het was een donkere nacht; een enkele voetganger ploegde door de sneeuw voor het ziekenhuis, zoekende achter zijn ondergesneeuwde wagen.  Achter de meeste venster in het gebouw achter hem brandde geen licht meer. <i>Logisch ook, op dit uur,</i> dacht hij. Toen hij net zijn auto had gevonden en wou instappen, hoorde hij uit datzelfde gebouw een hels gekrijs, alsof iemand dood ging. 
+	</p>
+		 
+	<div class="quote"> Onze wortelkes blijven wortelkes, ook als ze gekookt zijn.</div>
+		 
+	<p><i>Er kon ook iemand doodgaan,</i> schoot door hem heen. Het gebouw waar dat geschreeuw al enkele minuten uit kwam uit de kraamafdeling van dit ziekenhuis in Leuven.</p>
+
+</div>			
+
+</body>
+</html>
+  
---
+++ les1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>Les 1</title>
+
+	<link href="styles.css" rel="stylesheet" type= "text/css" >
+	<link href='http://fonts.googleapis.com/css?family=EB+Garamond&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<div id = "topbar" >
+
+	<?php  
+	$name = "les1";
+	include("topbar.php"); ?>
+	  
+</div>
+	
+<div class = "container" >
+		 
+		 <h1>Les 1</h1>
+</div>
+<div class = "imagecontainer">
+		 <img src="images/thermostat.jpg">
+			
+</div>
+
+
+</body>
+</html>
+  
