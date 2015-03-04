19a54669905fe2f71ef614abfc74f1b54f39183a
iPieter <pieter938@gmail.com>
2015-03-04 13:04:06 +0100

    Fixed fonts and updated text

--- les2.php
+++ les2.php
@@ -21,15 +21,7 @@
 		 
 		 <h1>Les 2</h1>
 		 
-		 <p>Het was een donkere nacht; een enkele voetganger ploegde door de sneeuw voor het ziekenhuis, zoekende achter zijn ondergesneeuwde wagen.  Achter de meeste venster in het gebouw achter hem brandde geen licht meer. Logisch ook, op dit uur, dacht hij. Toen hij net zijn auto had gevonden en wou instappen, hoorde hij uit datzelfde gebouw een hels gekrijs, alsof iemand dood ging. 
-		 </p>
-		 
-		 <div class="quote"> Onze wortelkes blijven wortelkes, ook als ze gekookt zijn.</div>
-		 
-		 <p>
-Er kon ook iemand doodgaan, schoot door hem heen. Het gebouw waar dat geschreeuw al enkele minuten uit kwam uit de kraamafdeling van dit ziekenhuis in Leuven.   
 
-			</p>
 			
 			
 </div>
